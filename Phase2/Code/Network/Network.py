106c706c98731f92b3705fc18565229f09481538
ShreyasKhobragade <shreyasdevdattakhobragade@gmail.com>
2025-02-05 14:59:07 -0500

    Merged all changes, New datagen, New Model and Report output and Image stitching

--- Phase2/Code/Network/Network.py
+++ Phase2/Code/Network/Network.py
@@ -10,6 +10,287 @@ Teaching Assistant in Robotics Engineering,
 Worcester Polytechnic Institute
 """
 
+# import torch.nn as nn
+# import sys
+# import torch
+# import numpy as np
+# import torch.nn.functional as F
+# import kornia  # You can use this to get the transform and warp in this project
+# import pytorch_lightning as pl
+
+# # Don't generate pyc codes
+# sys.dont_write_bytecode = True
+
+
+# # def LossFn(delta, img_a, patch_b, corners):
+# #     ###############################################
+# #     # Fill your loss function of choice here!
+# #     ###############################################
+
+# #     ###############################################
+# #     # You can use kornia to get the transform and warp in this project
+# #     # Bonus if you implement it yourself
+# #     ###############################################
+# #     loss_fn=nn.CrossEntropyLoss()
+# #     loss = ...
+# #     return loss
+
+# # def LossFn(PredicatedCoordinatesBatch, CoordinatesBatch):
+# #     # Reshape delta from (batch_size, 168) → (batch_size, 8, 21)
+# #     loss_fn=torch.nn.MSELoss()
+# #     loss=loss_fn(PredicatedCoordinatesBatch, CoordinatesBatch)
+
+# #     return loss
+
+# def LossFn(PredicatedCoordinatesBatch, CoordinatesBatch):
+#     # Ensure both inputs are float32
+#     PredicatedCoordinatesBatch = PredicatedCoordinatesBatch.float()
+#     CoordinatesBatch = CoordinatesBatch.float()
+
+#     # Compute loss using MSELoss
+#     loss_fn = torch.nn.MSELoss()
+#     loss = loss_fn(PredicatedCoordinatesBatch, CoordinatesBatch)
+
+#     return torch.sqrt(loss)
+
+
+
+# class HomographyModel(pl.LightningModule):
+#     def __init__(self):
+#         super(HomographyModel, self).__init__()
+#         # self.hparams = hparams
+#         self.model = SupNet()
+
+#     def forward(self, a):
+#         return self.model(a)
+
+#     def training_step(self, batch, batch_idx):
+#         img_a, patch_a, patch_b, corners, gt = batch
+#         delta = self.model(patch_a, patch_b)
+#         loss = LossFn(delta, img_a, patch_b, corners)
+#         logs = {"loss": loss}
+#         return {"loss": loss, "log": logs}
+
+#     def validation_step(self, batch, labels):
+#         prediction = self.model(batch)
+#         loss = LossFn(prediction, labels)
+#         # img_a, patch_a, patch_b, corners, gt = batch
+#         # delta = self.model(patch_a, patch_b)
+#         # loss = LossFn(delta, img_a, patch_b, corners)
+#         return {"val_loss": loss}
+
+#     def validation_epoch_end(self, outputs):
+#         avg_loss = torch.stack([x["val_loss"] for x in outputs]).mean()
+#         logs = {"val_loss": avg_loss}
+#         return {"avg_val_loss": avg_loss, "log": logs}
+
+
+# # class Net(nn.Module):
+# #     def __init__(self, InputSize, OutputSize):
+# #         """
+# #         Inputs:
+# #         InputSize - Size of the Input
+# #         OutputSize - Size of the Output
+# #         """
+# #         super().__init__()
+# #         #############################
+# #         # Fill your network initialization of choice here!
+# #         #############################
+# #         ...
+# #         #############################
+# #         # You will need to change the input size and output
+# #         # size for your Spatial transformer network layer!
+# #         #############################
+# #         # Spatial transformer localization-network
+# #         self.localization = nn.Sequential(
+# #             nn.Conv2d(1, 8, kernel_size=7),
+# #             nn.MaxPool2d(2, stride=2),
+# #             nn.ReLU(True),
+# #             nn.Conv2d(8, 10, kernel_size=5),
+# #             nn.MaxPool2d(2, stride=2),
+# #             nn.ReLU(True),
+# #         )
+
+# #         # Regressor for the 3 * 2 affine matrix
+# #         self.fc_loc = nn.Sequential(
+# #             nn.Linear(10 * 3 * 3, 32), nn.ReLU(True), nn.Linear(32, 3 * 2)
+# #         )
+
+# #         # Initialize the weights/bias with identity transformation
+# #         self.fc_loc[2].weight.data.zero_()
+# #         self.fc_loc[2].bias.data.copy_(
+# #             torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float)
+# #         )
+
+# #     #############################
+# #     # You will need to change the input size and output
+# #     # size for your Spatial transformer network layer!
+# #     #############################
+# #     def stn(self, x):
+# #         "Spatial transformer network forward function"
+# #         xs = self.localization(x)
+# #         xs = xs.view(-1, 10 * 3 * 3)
+# #         theta = self.fc_loc(xs)
+# #         theta = theta.view(-1, 2, 3)
+
+# #         grid = F.affine_grid(theta, x.size())
+# #         x = F.grid_sample(x, grid)
+
+# #         return x
+
+# #     def forward(self, xa, xb):
+# #         """
+# #         Input:
+# #         xa is a MiniBatch of the image a
+# #         xb is a MiniBatch of the image b
+# #         Outputs:
+# #         out - output of the network
+# #         """
+# #         #############################
+# #         # Fill your network structure of choice here!
+# #         #############################
+# #         return out
+
+# class SupNet(nn.Module):
+#     def __init__(self, InputSize=2, OutputSize=8):
+#         """
+#         Inputs:
+#         InputSize - Size of the Input
+#         OutputSize - Size of the Output (should be 168 for 21 bins * 8 dimensions)
+#         """
+
+#         super().__init__()
+        
+#         # VGG-style convolutional blocks for main network
+#         self.conv1_1 = nn.Conv2d(InputSize, 64, kernel_size=3, padding=1)
+#         self.bn1_1 = nn.BatchNorm2d(64)
+#         self.conv1_2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+#         self.bn1_2 = nn.BatchNorm2d(64)
+        
+#         self.conv2_1 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+#         self.bn2_1 = nn.BatchNorm2d(64)
+#         self.conv2_2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+#         self.bn2_2 = nn.BatchNorm2d(64)
+        
+#         self.conv3_1 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+#         self.bn3_1 = nn.BatchNorm2d(128)
+#         self.conv3_2 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+#         self.bn3_2 = nn.BatchNorm2d(128)
+        
+#         self.conv4_1 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+#         self.bn4_1 = nn.BatchNorm2d(128)
+#         self.conv4_2 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+#         self.bn4_2 = nn.BatchNorm2d(128)
+        
+#         # Max pooling layer
+#         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+#         # Fully connected layers for main network
+#         self.fc1 = nn.Linear(128 * 8 * 8, 1024)
+#         self.fc2 = nn.Linear(1024, OutputSize)
+        
+#         # Dropout layers
+#         self.dropout1 = nn.Dropout(0.5)
+#         self.dropout2 = nn.Dropout(0.5)
+        
+#         # Spatial transformer localization-network
+#         self.localization = nn.Sequential(
+#             nn.Conv2d(1, 8, kernel_size=7),
+#             nn.MaxPool2d(2, stride=2),
+#             nn.ReLU(True),
+#             nn.Conv2d(8, 10, kernel_size=5),
+#             nn.MaxPool2d(2, stride=2),
+#             nn.ReLU(True),
+#         )
+        
+#         # Regressor for the 3 * 2 affine matrix
+#         self.fc_loc = nn.Sequential(
+#             nn.Linear(10 * 3 * 3, 32),
+#             nn.ReLU(True),
+#             nn.Linear(32, 3 * 2)
+#         )
+        
+#         # Initialize the weights/bias with identity transformation
+#         self.fc_loc[2].weight.data.zero_()
+#         self.fc_loc[2].bias.data.copy_(
+#             torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float)
+#         )
+
+#     # def stn(self, x):
+#     #     """Spatial transformer network forward function"""
+#     #     xs = self.localization(x)
+#     #     xs = xs.view(-1, 10 * 3 * 3)
+#     #     theta = self.fc_loc(xs)
+#     #     theta = theta.view(-1, 2, 3)
+#     #     grid = F.affine_grid(theta, x.size())
+#     #     x = F.grid_sample(x, grid)
+#     #     return x
+
+#     def forward(self, x):
+#         """
+#         Input:
+#         xa is a MiniBatch of the image a
+#         xb is a MiniBatch of the image b
+#         Outputs:
+#         out - output of the network (168 dimensional for 21 bins * 8 corners)
+#         """
+
+#         #x = torch.cat([xa, xb], dim=1)
+        
+#         # First conv block
+#         x = F.relu(self.bn1_1(self.conv1_1(x)))
+#         x = F.relu(self.bn1_2(self.conv1_2(x)))
+#         x = self.pool(x)
+        
+#         # Second conv block
+#         x = F.relu(self.bn2_1(self.conv2_1(x)))
+#         x = F.relu(self.bn2_2(self.conv2_2(x)))
+#         x = self.pool(x)
+        
+#         # Third conv block
+#         x = F.relu(self.bn3_1(self.conv3_1(x)))
+#         x = F.relu(self.bn3_2(self.conv3_2(x)))
+#         x = self.pool(x)
+        
+#         # Fourth conv block
+#         x = F.relu(self.bn4_1(self.conv4_1(x)))
+#         x = F.relu(self.bn4_2(self.conv4_2(x)))
+#         x = self.pool(x)
+        
+#         # Flatten and apply dropout
+#         x = x.view(x.size(0), -1)
+#         x = self.dropout1(x)
+        
+#         # Fully connected layers with dropout
+#         #x = F.relu(self.fc1(x))
+#         x = self.fc1(x)
+#         out = self.fc2(x)
+        
+#         # Reshape to (batch_size, 8, 21) and apply softmax
+#         # x = x.view(-1, 8, 21)
+#         # x = F.softmax(x, dim=2)
+        
+#         # # Reshape back to (batch_size, 168)
+#         # out = x.view(-1, 168)
+        
+#         return out
+
+
+
+
+
+"""
+RBE/CS Fall 2022: Classical and Deep Learning Approaches for
+Geometric Computer Vision
+Project 1: MyAutoPano: Phase 2 Starter Code
+
+
+Author(s):
+Lening Li (lli4@wpi.edu)
+Teaching Assistant in Robotics Engineering,
+Worcester Polytechnic Institute
+"""
+
 import torch.nn as nn
 import sys
 import torch
@@ -182,11 +463,22 @@ class SupNet(nn.Module):
         self.conv4_2 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
         self.bn4_2 = nn.BatchNorm2d(128)
         
+        self.conv5_1 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.bn5_1 = nn.BatchNorm2d(256)
+        self.conv5_2 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.bn5_2 = nn.BatchNorm2d(256)
+
+        self.conv6_1 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.bn6_1 = nn.BatchNorm2d(256)
+        self.conv6_2 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.bn6_2 = nn.BatchNorm2d(256)
+
+
         # Max pooling layer
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         
         # Fully connected layers for main network
-        self.fc1 = nn.Linear(128 * 8 * 8, 1024)
+        self.fc1 = nn.Linear(16 * 8 * 8, 1024)
         self.fc2 = nn.Linear(1024, OutputSize)
         
         # Dropout layers
@@ -256,10 +548,18 @@ class SupNet(nn.Module):
         x = F.relu(self.bn4_1(self.conv4_1(x)))
         x = F.relu(self.bn4_2(self.conv4_2(x)))
         x = self.pool(x)
+
+        x = F.relu(self.bn5_1(self.conv5_1(x)))
+        x = F.relu(self.bn5_2(self.conv5_2(x)))
+        x = self.pool(x)
+
+        x = F.relu(self.bn6_1(self.conv6_1(x)))
+        x = F.relu(self.bn6_2(self.conv6_2(x)))
+        x = self.pool(x)
         
         # Flatten and apply dropout
         x = x.view(x.size(0), -1)
-        x = self.dropout1(x)
+        #x = self.dropout1(x)
         
         # Fully connected layers with dropout
         #x = F.relu(self.fc1(x))
--- Phase2/Code/try.py
+++ Phase2/Code/try.py
@@ -7,57 +7,275 @@ import skimage
 import PIL
 import sys
 import pandas as pd
+import torch
+import torchvision
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import datasets, transforms
+from torch.optim import AdamW
 
 
 
-def ReadLabels(LabelsPathTrain):
-    # Check if file exists
-    if not os.path.isfile(LabelsPathTrain):
-        print(f"ERROR: Train Labels do not exist in {LabelsPathTrain}")
-        sys.exit()
+# def ReadLabels(LabelsPathTrain):
+#     # Check if file exists
+#     if not os.path.isfile(LabelsPathTrain):
+#         print(f"ERROR: Train Labels do not exist in {LabelsPathTrain}")
+#         sys.exit()
     
-    # Read CSV file
-    df = pd.read_csv(LabelsPathTrain)
+#     # Read CSV file
+#     df = pd.read_csv(LabelsPathTrain)
     
-    # Extract the last 8 columns (h1 to h8) as a numpy array
-    TrainLabels = df.iloc[:, 2: 10].values
+#     # Extract the last 8 columns (h1 to h8) as a numpy array
+#     TrainLabels = df.iloc[:, 2: 10].values
     
-    return TrainLabels
+#     return TrainLabels
 
 
-LabelsPathTrain = '../Data/TrainSet/labels.csv'
-labels = ReadLabels(LabelsPathTrain)
+# LabelsPathTrain = '../Data/TrainSet/labels.csv'
+# labels = ReadLabels(LabelsPathTrain)
 
-# Print shape and first few rows for verification
-print("Shape of labels:", labels.shape)
-print("\nFirst few rows:" ,len(labels))
-print(labels[:3])
+# # Print shape and first few rows for verification
+# print("Shape of labels:", labels.shape)
+# print("\nFirst few rows:" ,len(labels))
+# print(labels[:3])
 
 
 
-# def ReadDirNames(ReadPath):
-#     """
-#     Inputs:
-#     ReadPath is the path of the CSV file you want to read
-#     Outputs:
-#     DirNames is a list of all patch_id values from the CSV file
-#     """
-#     # Check if the file exists
-#     if not os.path.isfile(ReadPath):
-#         print(f"ERROR: File does not exist at {ReadPath}")
-#         return None
+# # def ReadDirNames(ReadPath):
+# #     """
+# #     Inputs:
+# #     ReadPath is the path of the CSV file you want to read
+# #     Outputs:
+# #     DirNames is a list of all patch_id values from the CSV file
+# #     """
+# #     # Check if the file exists
+# #     if not os.path.isfile(ReadPath):
+# #         print(f"ERROR: File does not exist at {ReadPath}")
+# #         return None
     
-#     # Read the CSV file into a DataFrame
-#     df = pd.read_csv(ReadPath)
+# #     # Read the CSV file into a DataFrame
+# #     df = pd.read_csv(ReadPath)
     
-#     # Extract the 'patch_id' column as a list
-#     DirNames = df['patch_id'].tolist()
+# #     # Extract the 'patch_id' column as a list
+# #     DirNames = df['patch_id'].tolist()
     
-#     return DirNames
+# #     return DirNames
 
-# patch_ids = ReadDirNames(LabelsPathTrain)
+# # patch_ids = ReadDirNames(LabelsPathTrain)
     
-#     # Print the first few patch IDs for verification
-# if patch_ids:
-#     print("Number of patch IDs:", len(patch_ids))
-#     print("First few patch IDs:", patch_ids[:5])
+# #     # Print the first few patch IDs for verification
+# # if patch_ids:
+# #     print("Number of patch IDs:", len(patch_ids))
+# #     print("First few patch IDs:", patch_ids[:5])
+
+
+
+
+
+
+def find_best_match_and_patches(img1, img2):
+    # Convert images to grayscale if they're not already
+    if len(img1.shape) == 3:
+        gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+        gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    else:
+        gray1 = img1.copy()
+        gray2 = img2.copy()
+    
+    # Use SIFT with increased features
+    sift = cv2.SIFT_create(nfeatures=1000)
+    kp1, des1 = sift.detectAndCompute(gray1, None)
+    kp2, des2 = sift.detectAndCompute(gray2, None)
+    
+    # Use FLANN matcher
+    FLANN_INDEX_KDTREE = 1
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    search_params = dict(checks=100)
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    
+    matches = flann.knnMatch(des1, des2, k=2)
+    
+    # Apply ratio test
+    good_matches = []
+    for m, n in matches:
+        if m.distance < 0.6 * n.distance:
+            good_matches.append(m)
+    
+    # Sort and get best match
+    good_matches = sorted(good_matches, key=lambda x: x.distance)
+    best_match = good_matches[0]
+    
+    # Get patch coordinates
+    pt1 = kp1[best_match.queryIdx].pt
+    pt2 = kp2[best_match.trainIdx].pt
+    
+    patch_size = 128
+    half_size = patch_size // 2
+    
+    x1, y1 = int(pt1[0]), int(pt1[1])
+    x2, y2 = int(pt2[0]), int(pt2[1])
+    
+    # Ensure patch coordinates are within bounds
+    x1 = max(half_size, min(x1, gray1.shape[1] - half_size))
+    y1 = max(half_size, min(y1, gray1.shape[0] - half_size))
+    x2 = max(half_size, min(x2, gray2.shape[1] - half_size))
+    y2 = max(half_size, min(y2, gray2.shape[0] - half_size))
+    
+    # Extract patches (single channel)
+    patch1 = gray1[y1-half_size:y1+half_size, x1-half_size:x1+half_size]
+    patch2 = gray2[y2-half_size:y2+half_size, x2-half_size:x2+half_size]
+    
+    # Save patches if needed
+    cv2.imwrite('patch1.jpg', patch1)
+    cv2.imwrite('patch2.jpg', patch2)
+    
+    return patch1, patch2, (x1, y1), (x2, y2)
+
+def calculate_dlt(H4pt, CA, device='cuda'):
+    # Convert numpy arrays to torch tensors on GPU
+    H4pt = torch.tensor(H4pt, device=device).float()
+    CA = torch.tensor(CA, device=device).float()
+    
+    # Reshape the input arrays
+    H4pt = H4pt.reshape(4, 2)
+    CA = CA.reshape(4, 2)
+    
+    # Initialize matrix A for DLT equations
+    A = torch.zeros((8,9), device=device)
+    
+    # Fill matrix A with equations
+    for i in range(4):
+        x, y = CA[i]
+        u, v = H4pt[i] - CA[i]
+        
+        A[2*i] = torch.tensor([-x, -y, -1, 0, 0, 0, u*x, u*y, u], device=device)
+        A[2*i + 1] = torch.tensor([0, 0, 0, -x, -y, -1, v*x, v*y, v], device=device)
+    
+    # Solve using SVD
+    _, _, Vt = torch.linalg.svd(A)
+    
+    # Get last row and reshape
+    H = Vt[-1].reshape(3, 3)
+    
+    # Normalize
+    H = H / H[2, 2]
+    
+    return H.cpu().numpy()
+
+def stitch_images(img1, img2, H):
+    # Store original images for color conversion later
+    orig_img1 = img1.copy()
+    orig_img2 = img2.copy()
+    
+    # Convert to single channel if not already
+    if len(img1.shape) == 3:
+        img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    if len(img2.shape) == 3:
+        img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    
+    h1, w1 = img1.shape[:2]
+    h2, w2 = img2.shape[:2]
+    
+    # Create corners matrix
+    corners1 = np.float32([[0, 0], [0, h1], [w1, h1], [w1, 0]]).reshape(-1, 1, 2)
+    
+    # Transform corners
+    corners1_transformed = cv2.perspectiveTransform(corners1, H)
+    
+    # Calculate output size
+    all_corners = np.vstack((
+        corners1_transformed,
+        np.float32([[0, 0], [0, h2], [w2, h2], [w2, 0]]).reshape(-1, 1, 2)
+    ))
+    
+    x_min, y_min = np.int32(np.min(all_corners, axis=0)[0] - 0.5)
+    x_max, y_max = np.int32(np.max(all_corners, axis=0)[0] + 0.5)
+    
+    # Translation matrix
+    translation = np.array([
+        [1, 0, -x_min],
+        [0, 1, -y_min],
+        [0, 0, 1]
+    ])
+    
+    H_final = translation @ H
+    
+    # Warp and blend
+    output_size = (x_max - x_min, y_max - y_min)
+    
+    # Warp the grayscale image first
+    warped_gray = cv2.warpPerspective(img1, H_final, output_size)
+    
+    # Create binary mask from warped grayscale image
+    mask = (warped_gray > 0).astype(np.uint8)
+    
+    # Warp the color image
+    warped_color = cv2.warpPerspective(orig_img1, H_final, output_size)
+    
+    # Create output canvas (color)
+    output = np.zeros((output_size[1], output_size[0], 3), dtype=np.uint8)
+    
+    # Copy second color image
+    output[-y_min:h2-y_min, -x_min:w2-x_min] = orig_img2
+    
+    # Blend using mask
+    mask = np.stack([mask] * 3, axis=2)
+    output = np.where(mask > 0, warped_color, output)
+    
+    return output
+
+def main_stitching_pipeline(img1, img2, model, device='cuda'):
+    # Get patches
+    patch1, patch2, (x1, y1), (x2, y2) = find_best_match_and_patches(img1, img2)
+    
+    # Stack patches and prepare for model
+    stacked_patches = np.stack((patch1, patch2), axis=2)  # Stack along channel dimension
+    stacked_patches = torch.from_numpy(stacked_patches).float().to(device)
+    stacked_patches = stacked_patches.unsqueeze(0)  # Add batch dimension
+    stacked_patches = stacked_patches.permute(0, 3, 1, 2).contiguous()  # [B, C, H, W]
+    stacked_patches = stacked_patches / 255.0  # Normalize
+    print(stacked_patches.shape)
+    
+    # Get H4pt from model
+    with torch.no_grad():
+        H4pt = model(stacked_patches)
+        H4pt = H4pt.cpu().numpy().squeeze()
+    
+    # Calculate H matrix using DLT
+    CA = np.array([[0,0], [0,128], [128,128], [128,0]])
+    H_patch = calculate_dlt(H4pt, CA, device)
+    
+    # Transform to image coordinates
+    offset_matrix1 = np.array([[1, 0, x1], [0, 1, y1], [0, 0, 1]])
+    offset_matrix2 = np.array([[1, 0, -x2], [0, 1, -y2], [0, 0, 1]])
+    
+    H_full = offset_matrix2 @ H_patch @ offset_matrix1
+    
+    # Stitch images
+    result = stitch_images(img1, img2, H_full)
+    return result
+
+def main():
+    # Set device
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
+    # Load your model
+    model = HomographyModel().to(device)  # Replace with your model architecture
+    #model.load_state_dict(torch.load('../Checkpoints/0a100model.ckpt'))
+    model_checkpoint = torch.load('../Checkpoints_UnSup/49model.ckpt')
+    model.load_state_dict(model_checkpoint['model_state_dict'])
+    #model = model.to(device)
+    model.eval()
+    
+    # Load images
+    img1 = cv2.imread('2.jpg')
+    img2 = cv2.imread('3.jpg')
+    
+    # Perform stitching
+    result = main_stitching_pipeline(img1, img2, model, device)
+    
+    # Save result
+    cv2.imwrite('stitched_result_unsup.jpg', result)
+    print(result.shape)
+
+if __name__ == "__main__":
+    main()
